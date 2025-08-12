4714ac2138a04f8a1f36d12daf7017051ce9a33f
zougmar <zougmar123@gmail.com>
2025-08-12 18:12:46 +0100

    exercice7

---
+++ Week1/day2/exercices/exercice6.py
@@ -0,0 +1,5 @@
+def make_shirt(size="Large", text="I love Python"):
+    print(f"The size of the shirt is {size} and the text is {text}")
+make_shirt()    
+make_shirt(size="Medium")
+make_shirt(size="Small", text="Python is great")
