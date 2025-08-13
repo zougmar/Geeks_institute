62466b22725076c3278fa1a7a4ae75b820a348ef
zougmar <zougmar123@gmail.com>
2025-08-13 10:31:00 +0100

    exe7 second commit

--- Week1/day2/exercices/exercice7.py
+++ Week1/day2/exercices/exercice7.py
@@ -7,3 +7,14 @@ for _ in range(5):
  def main():
     store_temps = get_random_temp()
     print(f"The temperature right now is : {store_temps}°C")
+    if store_temps < 0:
+        print("Brrr, that’s freezing! Wear some extra layers today")
+    elif 0 <= store_temps <= 16:
+        print("Quite chilly! Don’t forget your coat")
+    elif 17 <= store_temps <= 23:
+        print("the weather is nice. A light jacket should be enough.")
+    elif 24 <= store_temps <= 32:
+        print("The weather is warm. A t-shirt is fine.")
+    elif 34 <= store_temps <= 40:
+        print("It’s hot! Stay hydrated and wear sunscreen.")
+main()
