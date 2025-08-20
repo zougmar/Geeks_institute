24849010284ed5d6af9e2fca34f77638fa4543ca
zougmar <zougmar123@gmail.com>
2025-08-20 15:07:45 +0100

    c

---
+++ Students_Connections.session.sql
@@ -0,0 +1,7 @@
+INSERT INTO students (id, first_name, last_name, birth_date)
+VALUES (
+    id:integer,
+    'first_name:character varying',
+    'last_name:character varying',
+    'birth_date:date'
+  );
---
+++ week2/day2/exercice1.sql
@@ -0,0 +1,61 @@
+CREATE TABLE items (
+item_id SERIAL PRIMARY KEY,
+item_name VARCHAR(100),
+price NUMERIC(10,2)
+);
+CREATE TABLE customers (
+customer_id SERIAL PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50)
+);
+INSERT INTO customers (first_name, last_name) VALUES
+('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson');
+
+INSERT INTO items (item_name, price) VALUES
+('Small Desk', 100),
+('Large Desk', 300),
+('Fan', 80);
+
+
+--All the customers
+SELECT * FROM customers; 	
+
+--All the items.
+SELECT * FROM items; 
+
+--All the items with a price above 80 (80 not included).
+SELECT * FROM items 
+WHERE price > 80;
+
+--All the items with a price below 300. (300 included)
+SELECT * FROM items 
+WHERE price < 300;
+
+--All customers whose last name is ‘Smith’ (What will be your outcome?).
+SELECT * FROM customers
+WHERE last_name = 'Smith';
+
+--All customers whose last name is ‘Jones’.
+SELECT * FROM customers 
+WHERE last_name = 'Jones';
+
+--All customers whose firstname is not ‘Scott’.
+SELECT * FROM customers
+WHERE first_name <> 'Scott';
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+SELECT * FROM items ORDER BY price desc;
+SELECT * FROM items 
+--The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+SELECT first_name
+FROM customers
+ORDER BY first_name ASC
+LIMIT 3;
+--All last names (no other columns!), in reverse alphabetical order (Z-A)
+SELECT first_name
+FROM customers
+ORDER BY first_name DESC
+LIMIT 3;
