d3cc1f9d467483232ae702dbd59273d1c5ad64bd
zougmar <zougmar123@gmail.com>
2025-08-22 11:10:11 +0100

    c

---
+++ week2/exercices/day2/daily_challange/sql.sql
@@ -0,0 +1,14 @@
+--Q1. What will be the OUTPUT of the following statement?
+
+Output: 0 rows
+
+-- Q2. What will be the OUTPUT of the following statement?
+output : 2 rows
+
+-- Q3. What will be the OUTPUT of the following statement?
+
+output : 0 rows
+
+-- Q4. What will be the OUTPUT of the following statement?
+
+output : 2 rows
