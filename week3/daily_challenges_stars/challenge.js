30659377358dce9436f318bf5461a961b7e7a543
zougmar <zougmar123@gmail.com>
2025-09-08 14:18:56 +0100

    c

--- week3/daily_challenges_stars/challenge.js
+++
@@ -1,6 +0,0 @@
-pattern = ''
-
-for (let i = 0; i < 5; i++) {
-    pattern += '*'
-    console.log(pattern)
-}
--- week3/exercices/exercice3.js
+++
@@ -1,8 +0,0 @@
-let num = Number(prompt("Enter a number:"));  
-
-while (num < 10) {
-  num = Number(prompt("The number is too small. Enter a new number:"));
-}
-
-console.log(`Great! You entered ${num}, which is >= 10`);
-typeof prompt("test"); 
