5826aeb4134aa120f875dec310558629943d3893
zougmar <zougmar123@gmail.com>
2025-09-14 10:04:36 +0100

    c

---
+++ week4/day2/daily_challenges_car_inventure/challenge.js
@@ -0,0 +1,22 @@
+// Part 1
+function getCarHonda(carInventory) {
+
+  const car = carInventory.find(c => c.car_make === "Honda");
+
+ 
+  return `This is a ${car.car_make} ${car.car_model} from ${car.car_year}.`;
+}
+
+
+const inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+
+console.log(getCarHonda(inventory));
+
+
+// Part 2
