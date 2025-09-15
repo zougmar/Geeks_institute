56c2c6c33d351376600f056a8ed9c1e6d66fa48b
zougmar <zougmar123@gmail.com>
2025-09-15 14:21:53 +0100

    c

---
+++ week4/day4/exercice xp/exercice3.js
@@ -0,0 +1,31 @@
+const marioGame = {
+  detail : "An amazing game!",
+  characters : {
+      mario : {
+        description:"Small and jumpy. Likes princesses.",
+        height: 10,
+        weight: 3,
+        speed: 12,
+      },
+      bowser : {
+        description: "Big and green, Hates princesses.",
+        height: 16,
+        weight: 6,
+        speed: 4,
+      },
+      princessPeach : {
+        description: "Beautiful princess.",
+        height: 12,
+        weight: 2,
+        speed: 2,
+      }
+  },
+}
+
+
+const marioJSON = JSON.stringify(marioGame);
+console.log(marioJSON);
+
+
+const prettyJSON = JSON.stringify(marioGame, null, 2);
+console.log(prettyJSON);
