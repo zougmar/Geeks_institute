32b436ff1cde84b1524955547d634c01f92e3a12
zougmar <zougmar123@gmail.com>
2025-10-01 13:08:52 +0100

    chalk

---
+++ week5/day4/exercices xp/exercice6/npm-beginner/app.js
@@ -0,0 +1,7 @@
+// app.js
+import chalk from 'chalk';
+
+console.log(chalk.green('✅ Welcome to the world of Node.js!'));
+console.log(chalk.blue.bold('This text is blue and bold'));
+console.log(chalk.red.italic('🚀 Learning NPM is super easy!'));
+console.log(chalk.bgYellow.black('Yellow background with black text'));
