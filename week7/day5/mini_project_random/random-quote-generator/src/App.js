d7ef59925a9d7590e353a2acab75b16029776247
zougmar <zougmar123@gmail.com>
2025-10-19 13:09:18 +0100

    c

---
+++ week7/day5/mini_project_random/random-quote-generator/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import QuoteBox from "./component/QuoteBox";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-200 flex items-center justify-center p-6">
+      <QuoteBox />
+    </div>
+  );
+}
