4525b5e6f94e11b41a7e267b3652c40f5db6d834
zougmar <zougmar123@gmail.com>
2025-10-23 12:19:56 +0100

    c

---
+++ week9/day1/exercice_xp/exercie1/hello.js
@@ -0,0 +1,10 @@
+// Declare a string variable with type annotation
+var message = "Hello, World!";
+// Log the message to the console
+console.log(message);
+// Declare variables of different types
+var count = 5;
+var isActive = true;
+// Log these variables
+console.log("Count:", count);
+console.log("Active:", isActive);
